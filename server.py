ebe3cce3febcf2d614bc03475934af67e0133242
Sara-Vass <svasscs@gmail.com>
2019-10-22 12:33:53 -0700

    got file upload working

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ from jinja2 import StrictUndefined
 from flask import (Flask, render_template, redirect, request, flash,
                     session, jsonify)
 
+from boxsdk import Client, OAuth2
+
+import os
+
 # from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -24,13 +28,65 @@ def upload():
 
     file = request.files['image_uploads']
 
-    print(file)
+    file_read = file.stream.read()
+
+    #Get path to root of app
+    APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+    #save img
+    destination = "/".join([APP_ROOT, file.filename])
+
+
+    #create image file
+    my_file = open(destination, 'wb')
+    #write blob data to image file
+    my_file.write(file_read)
+
+    #close file
+    my_file.close()
+
+    # path = os.path.realpath(file.name)
+
+    # print(path)
+    print(file.filename)
+
+    client = make_client()
+
+    print(client)
+
+    folder_id = '0'
+    print(client.folder(folder_id))
+    uploaded_file = client.folder(folder_id).upload(destination)
+
+    os.remove(destination)
     
-    return 'yay'
+    return redirect('/')
 
 
-    # folder_id = '22222'
-    # uploaded_file = client.folder(folder_id).upload('/path/to/file.pdf')
+
+# def upload_file(client):
+#     root_folder = client.folder(folder_id='0')
+#     file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'file.txt')
+#     a_file = root_folder.upload(file_path, file_name='i-am-a-file.txt')
+#     try:
+#         print('{0} uploaded: '.format(a_file.get()['name']))
+#     finally:
+#         print('Delete i-am-a-file.txt succeeded: {0}'.format(a_file.delete()))
+
+
+def make_client():
+
+    auth = OAuth2(
+        client_id='{CLIENT_ID}'.format(CLIENT_ID=os.environ.get('CLIENT_ID')),
+        client_secret='{CLIENT_SECRET}'.format(CLIENT_SECRET=os.environ.get('CLIENT_SECRET')),
+        access_token='{DEV_TOKEN}'.format(DEV_TOKEN=os.environ.get('DEV_TOKEN')),
+    )
+    client = Client(auth)
+
+    user = client.user().get()
+    print('The current user ID is {0}'.format(user.id))
+
+    return client
 
 
 if __name__ == "__main__":
@@ -44,5 +100,6 @@ if __name__ == "__main__":
 
     # Use the DebugToolbar
     # DebugToolbarExtension(app)
+
 
     app.run(port=5000, host='0.0.0.0')
